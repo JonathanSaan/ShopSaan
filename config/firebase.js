474630f2104b70094d7d8945ce81520c5d31bb3f
JonathanSaan <crosspool03@gmail.com>
2022-08-09 15:42:55 -0300

    up

--- config/firebase.js
+++ config/firebase.js
@@ -7,7 +7,8 @@ const firebaseConfig = {
 	storageBucket: `${process.env.NEXT_PUBLIC_Storage_Bucket}`,
 	databaseURL: `${process.env.NEXT_PUBLIC_Database_URL}`,
 	messagingSenderId: `${process.env.NEXT_PUBLIC_Message_Sender_Id}`,
-	appId: `${process.env.NEXT_PUBLIC_App_Id}`
+	appId: `${process.env.NEXT_PUBLIC_App_Id}`,
+	measurementId: `${process.env.NEXT_PUBLIC_Measurement_Id}`
 };
 
 export const app = initializeApp(firebaseConfig);
