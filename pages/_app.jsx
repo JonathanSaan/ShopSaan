99f62e2a36ab577132259a1bdf796680d27e9691
JonathanSaan <crosspool03@gmail.com>
2023-03-29 13:19:36 -0300

    up

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,8 +4,6 @@ import { CartProvider } from "react-use-cart";
 
 import "../styles/globals.scss";
 
-export { default } from "next/app";
-
 function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState(false);
 
